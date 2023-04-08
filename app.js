aff8e3c80b0fc2186ebb4d2d81fdc17124e02c50
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2023-04-08 04:42:46 +0200

    installation de mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+//base de donnee, a placer apres la declaration de app
+mongoose
+  .connect(
+    "mongodb+srv://Aleks:ClusterOC0passwordmachinebrokempd21%21@clusteroc0.wldhxua.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 //middleware pour interpreter le json du front, a declarer apres la declaration de app
 app.use(express.json());
 
