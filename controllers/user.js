2509608fda52b99519bfbedc367a7970ef9446d3
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2023-04-13 07:10:52 +0200

    added auth token

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   bcrypt
@@ -21,21 +22,23 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res
-          .status(401)
-          .json({ message: "Paire login/mot de passe incorrecte" });
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
       }
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res
-              .status(401)
-              .json({ message: "Paire login/mot de passe incorrecte" });
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
           }
           res.status(200).json({
             userId: user._id,
-            token: "TOKEN",
+            token: jwt.sign(
+              { userId: user._id },
+              "asdsadfhasdfhasdhadfhasdfbabwrsbersdtvsdzszdfsrtydeastzghsxrethbsrdxfhtjnbdxhtyfgjnmyfghukm,gkl,jh.khj.gdazsergeazsrghbazsedfvgrbszedghbsbrtdfhsrdfxthdxtyjET",
+              {
+                expiresIn: "24h",
+              }
+            ),
           });
         })
         .catch((error) => res.status(500).json({ error }));
